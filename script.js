5f08989d443b6699c78be3669046e26cc8ee2616
Andersson Silva Gonçalves <anderssonsilva793@gmail.com>
2023-10-14 17:49:56 -0300

    adição de novas funções no conversor

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-//Corveter de horas para dias
+const outDias = document.getElementById('dias');
+const outMinutos = document.getElementById('minutos');
+const outSegundos = document.getElementById('segundos');
+
+//chamas as respectivas funcões para alterar o HTML
+function chamarFuncoes(e) {
+  outDias.innerHTML = horasParaDias(e);
+  outMinutos.innerHTML = horasParaMinutos(e);
+  outSegundos.innerHTML = horasParaSegundos(e);
+}
+
+//corveter horas para dias
 function horasParaDias(horas) {
   var res = horas / 24;
 
@@ -7,4 +18,16 @@ function horasParaDias(horas) {
   return res;
 }
 
-//  res.innerHTML = `${(horas / 24).toFixed(2)} Dias`;
+//converter horas para minutos
+function horasParaMinutos(horas) {
+  var res = horas * 60;
+
+  return res;
+}
+
+//converter horas para segundos
+function horasParaSegundos(horas) {
+  var res = horas * 60 * 60;
+
+  return res;
+}
